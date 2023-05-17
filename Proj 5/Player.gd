021df80e7db190f012f36adc34f018b2e7a96d9b
gripotter <87414790+gripotter@users.noreply.github.com>
2023-05-17 13:06:39 +0900

    2023/05/17 13:05 Finish Before Creating the Enemy and Make Mob scene a little

--- Proj 5/Player.gd
+++ Proj 5/Player.gd
@@ -1,3 +1,52 @@
 extends Area2D
+signal hit
 @export var speed=400
 var screen_size
+
+func _ready():
+	screen_size = get_viewport_rect().size
+	hide()#should erase when finish making the game
+func _process(delta):
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+	
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite2D.play()
+	else:
+		$AnimatedSprite2D.stop()
+		
+	if velocity.x != 0:
+		if velocity.y != 0:
+			$AnimatedSprite2D.animation = "up"
+			$AnimatedSprite2D.flip_v = velocity.y > 0
+			$AnimatedSprite2D.flip_h = velocity.x < 0
+		else:
+			$AnimatedSprite2D.animation = "walk"
+			$AnimatedSprite2D.flip_v = false
+			$AnimatedSprite2D.flip_h = velocity.x < 0
+			
+	elif velocity.y != 0:
+		$AnimatedSprite2D.animation = "up"
+		$AnimatedSprite2D.flip_v = velocity.y > 0
+	
+	position += velocity * delta
+	position.x = clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)
+
+
+func _on_body_entered(body):
+	hide()
+	hit.emit()
+	$CollisionShape2D.set_deferred("disabled", true)#이거 왜 넣는지 이해가 안 됨 물어봐야 할 듯? 
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
